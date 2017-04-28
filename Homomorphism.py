840572e6777bf2f0eac0c3e41bcaef9c97f0b5d0
haohaowasky <alan91wang@gmail.com>
2017-04-28 13:40:12 -0400

    Isomorphic
    This is the program that given 2 strings and returns a boolean tells if
    they are isomorphic

---
+++ Homomorphism.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        check_index = []
+        counter = 0
+        ref = len(s)
+        while counter != ref:
+            repeat_index = [i for i, x in enumerate(s) if x == s[counter]]
+            repeat_index2 = [j for j, k in enumerate(t) if k == t[counter]]
+            if repeat_index != repeat_index2:
+                return False
+            counter += 1
+        return True
