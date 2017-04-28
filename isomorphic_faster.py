ec11e4fddaa2eb22f90908aef38623c9857f0d7a
haohaowasky <alan91wang@gmail.com>
2017-04-28 15:37:33 -0400

    faster version
    This is the faster version of isomorphic. Instead of scanning the whole
    array everything for each character. This only scans the first digits
    of array until it doesn’t meet requirement.

---
+++ isomorphic_faster.py
@@ -0,0 +1,35 @@
+
+def isIsomorphic(s, t):
+    check_index = []
+    check_index2 = []
+    k = 0
+    j = 0
+    counter = 0
+    ref = len(s)
+    while counter != ref:
+        if s[counter] in s[0:(counter)]:
+            print(s[0:(counter)])
+            check_index.append(check_index[s.index(s[counter])])
+        else:
+            k += 1
+            check_index.append(k)
+            print(check_index)
+            
+        if t[counter] in t[0:(counter)]:
+            print(t[0:(counter)])
+
+            check_index2.append(check_index2[t.index(t[counter])])
+        else:
+            j +=1
+            check_index2.append(j)
+            print(check_index2)
+            
+        if check_index != check_index2:
+            return False
+        counter += 1
+    return True
+
+s = "apple"
+t = "pipul"
+
+isIsomorphic(s, t)
